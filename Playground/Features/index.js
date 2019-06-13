cc7248ff9a9edc0c44d24b14e13c9a59e459c980
Pavneet Singh Saund <pav.saund@gmail.com>
2019-06-13 22:58:46 +0200

    Add Playground workspace to locally test contrib helpers

---
+++ Playground/Features/index.js
@@ -0,0 +1,11 @@
+import { observable } from "aurelia-binding";
+
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Dolittle. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+export class index {
+
+  @observable today = new Date();
+}
---
+++ Playground/Features/main.js
@@ -0,0 +1,22 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Dolittle. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+import environment from './environment';
+import { PLATFORM } from 'aurelia-pal';
+import * as Bluebird from 'bluebird';
+
+// remove out if you don't want a Promise polyfill (remove also from webpack.config.js)
+Bluebird.config({ warnings: { wForgottenReturn: false } });
+
+export function configure(aurelia) {
+    aurelia.use
+        .standardConfiguration()
+        .plugin(PLATFORM.moduleName('@dolittle/aurelia'))
+        .plugin(PLATFORM.moduleName('@dolittle/aurelia.contrib'));
+
+    if (environment.debug) {
+        aurelia.use.developmentLogging();
+    }
+    aurelia.start().then(() => aurelia.setRoot(PLATFORM.moduleName('app')));
+}
